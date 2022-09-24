6b79ee938751d2e20a7173aeeeb62d9719b123d2
Eduardo Alveo <eduardo.developer@javialveo.com>
2022-09-23 20:50:42 -0500

    nueva implementación de main.py, se ha separado la interfaz de usuario.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
-print("Reiniciando mi proyecto CliPhoneBook")
+import ui as mui
+
+APP_NAME = "cliPhoneBook"
+APP_VERSION = "0.3.0"
+APP_TITLE = f"{APP_NAME} v{APP_VERSION}"
+
+def main():
+  myUI = mui.UserInterface(APP_TITLE)
+  
+  userOption = 0
+  endProgram = myUI.getExitOption()
+  
+  while userOption != endProgram:
+    try:
+      myUI.mainMenu()
+      
+      readOption = input(f"Opción [1:{endProgram}] ")
+      userOption = int(readOption)
+      
+    except:
+      print(f"\n\nPor favor ingresa un número entre 1 y {endProgram}")
+      input("Pulsa ENTER para continuar")
+
+main()
