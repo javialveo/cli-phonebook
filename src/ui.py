f1aafe32be4155e4e5f2a34ef76015b057ec1a69
Eduardo Alveo <eduardo.developer@javialveo.com>
2022-09-23 21:41:33 -0500

    ui implementa una clase para el manejo del menú principal y las diferentes opciones del programa.

---
+++ src/ui.py
@@ -0,0 +1,26 @@
+class UserInterface:
+  def __init__(self, titleApp):
+    self.__menuList = [
+      "1. Mostrar lista de contactos",
+      "2. Registrar contacto",
+      "3. Actualizar contacto",
+      "4. Eliminar contacto",
+      "5. Eliminar lista de contactos",
+      "6. Salir"
+    ]
+    
+    self.__lineBreak = "\n" * 20
+    self.__titleApp = titleApp
+  
+  def getExitOption(self):
+    exitOption = len(self.__menuList)
+    return exitOption
+  
+  def mainMenu(self):
+    print(self.__lineBreak)
+    print(f"\t\t{self.__titleApp}")
+    print("\tAgenda de contactos en línea de comandos")
+    
+    print("\nEscoge una de las siguientes opciones disponibles")
+    for menu in self.__menuList:
+      print(menu)
