8d579c783ab81c2d66603c38ce40aeeda58fc2ca
Eduardo Alveo <eduardo.developer@javialveo.com>
2022-09-25 01:13:53 -0500

    Se agrega la función getAllData, que realiza una consulta a la base de datos y devuelve los datos presentes en ella.

--- src/database.py
+++ src/database.py
@@ -32,15 +32,38 @@ class Database:
   
   def createTable(self):
     columns = ""
-
+    
     for key, values in self.__columnList.items():
       columns += f"{key} {values}, "
     
     sqlStatement = f"create table {self.__tableName} ({columns[:-2]})"
-
+    
     db = mdb.connect(f"{self.__databasePath}/{self.__namedb}.db")
     cur = db.cursor()
     cur.execute(sqlStatement)
     db.commit()
     db.close()
+  
+  def getAllData(self, orderBy=None):
+    db = mdb.connect(f"{self.__databasePath}/{self.__namedb}.db")
+    cur = db.cursor()
+    
+    if orderBy != None:
+      cur.execute(f"select name, lastName, phoneNumber, email from {self.__tableName} order by {orderBy}")
+    else:
+      cur.execute(f"select name, lastName, phoneNumber, email from {self.__tableName}")
+    
+    data = cur.fetchall()
+    
+    db.commit()
+    db.close()
+    
+    if len(data) > 0:
+      return data
+    else:
+      return None
 
+bd = Database("cliphonebook", "database")
+datos = bd.getAllData()
+
+print(datos)
